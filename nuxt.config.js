4593bd17c739d2f9774e6519f865eba3169a51cb
Kevin Lewis <kvn@lws.io>
2019-06-14 16:09:09 +0100

    Open graph bits

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,12 +9,14 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Hacksmiths is the student-run tech society at Goldsmiths, University of London.' }
+      { hid: 'description', name: 'description', content: 'Hacksmiths is the student-run tech society at Goldsmiths, University of London.' },
+      { property: 'description', content: 'Hacksmiths is the student-run tech society at Goldsmiths, University of London.' },
+      { property: 'og:image', content: 'https://goldsmiths.tech/og.png' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Open+Sans:300,400,700&display=swap' },
-      { rel: 'stylesheet', href: "https://use.fontawesome.com/releases/v5.3.1/css/all.css", crossorigin: 'anonymous', integrity: 'sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU' }
+      { rel: 'stylesheet', href: "https://use.fontawesome.com/releases/v5.3.1/css/all.css", crossorigin: 'anonymous', integrity: 'sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU' },
     ]
   },
   /*
