8ac1cec2482c3dbc157edf62787389f5479eb799
Kevin Lewis <kvn@lws.io>
2019-06-14 16:03:57 +0100

    Contact

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>
-    Contact
+  <div id='contact' class='container'>
+    <h1>Contact Hacksmiths</h1>
+    <ul>
+      <li><a href="#">Facebook Page</a></li>
+      <li><a href="#">Facebook Group</a></li>
+      <li><a href="#">Messenger Bot</a></li>
+      <li><a href="#">Twitter</a></li>
+      <li><a href="#">Instagram</a></li>
+      <li><a href="#">Email</a></li>
+    </ul>
   </div>
 </template>
+ 
+ <style scoped>
+ ul {
+   margin-left: 1.25em;
+ }
+ li {
+   margin-top: 1em;
+ }
+ </style>
+ 
