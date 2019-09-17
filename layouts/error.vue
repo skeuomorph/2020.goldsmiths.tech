52fd51a646cd31908bfa5889eda494025c280beb
Dennis Duka <GreyTeddy@users.noreply.github.com>
2019-09-17 10:16:43 +0100

    added custom error/redirect page

---
+++ layouts/error.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <h1 v-if="error.statusCode === 404">Page not found or we haven't added one yet. 
+        <br />We're sorry.</h1>
+    <h1 v-else>An error occurred</h1>
+    <nuxt-link to="/">Back to Home page</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error'],
+  layout: 'blog' // you can set a custom layout for the error page
+}
+</script>
+
+<style scoped>
+.container {
+    text-align: center;
+}
+</style>
